2d13876a6914f6d3179396f97bc022e47d0ddf7c
dayadam <dayadamuga@gmail.com>
2019-11-18 20:16:04 -0500

    bootstrap added to home and static server made

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const path = require("path");
+app = express();
+const PORT = (process.env.PORT || 3000);
 
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
+app.use(express.static('app/public/'));
