7ca8217640470fb72e126cd720749470c39edd4f
dayadam <dayadamuga@gmail.com>
2019-11-19 22:02:02 -0500

    post route json

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,19 @@
+const path = require("path");
+const friendsData = require(path.join(__dirname, "../data/friends.js"));
+
 module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
-        console.log(req.body);
-        res.send(req.body);
+        const newFriend = req.body;
+        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+        console.log(newFriend);
+        friendsData.push(newFriend);
+        res.json(newFriend);
+
+    });
+
+    app.get("/api/friends", function (req, res) {
+        return res.json(friendsData)
     });
 
 }
